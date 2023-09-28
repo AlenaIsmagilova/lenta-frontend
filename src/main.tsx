1396f113202d9def62cf3665d9e1e6ce4f6540f6
Alena Ismagilova <ismagilovaalena90@yandex.ru>
2023-09-28 12:48:30 +0400

    add:react-router-dom, fix:structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { setupStore } from "./app/store";
-import App from "./app/App";
 import "./index.css";
+import SignIn from "./pages/SignIn/SignIn";
 
 const store = setupStore();
+
+const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <SignIn />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
