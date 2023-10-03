99998251ae23e7e0547c9f66476a127906f0d262
Alena Ismagilova <ismagilovaalena90@yandex.ru>
2023-10-03 11:14:33 +0400

    add:table with css styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,36 +1,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {Provider} from "react-redux";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
-import {setupStore} from "./app/store";
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { setupStore } from "./app/store";
 import SignIn from "./pages/SignIn/SignIn";
-import {CssBaseline, ThemeProvider} from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import lentaTheme from "./app/assets/styles/theeme";
+import BasicTable from "./components/Table/Table";
+import "./app/assets/styles/styles.css";
 
 const store = setupStore();
 
 const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <></>,
-    },
-    {
-        path: "/signin",
-        element: <SignIn/>,
-    },
-    {
-        path: "/statistics",
-        element: <></>,
-    },
+  {
+    path: "/",
+    element: <BasicTable />,
+  },
+  {
+    path: "/signin",
+    element: <SignIn />,
+  },
+  {
+    path: "/statistics",
+    element: <></>,
+  },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <Provider store={store}>
-            <ThemeProvider theme={lentaTheme}>
-                <CssBaseline/>
-                <RouterProvider router={router}/>
-            </ThemeProvider>
-        </Provider>
-    </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={lentaTheme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </Provider>
+  </React.StrictMode>
 );
