2515d803d995778231b71227a5e87c8706b42ebf
Maria Dobrorodnova <maria.dobrorodnova@gmail.com>
2023-10-07 15:23:46 +0300

    fix: access to data properties

--- src/components/ProfileInfo/ProfileInfo.tsx
+++ src/components/ProfileInfo/ProfileInfo.tsx
@@ -6,17 +6,17 @@ import phone from "../../app/images/phone.svg";
 import settings from "../../app/images/settings.svg";
 import logout from "../../app/images/logout.svg";
 import styles from "./ProfileInfo.module.css";
-import { useEffect, useState } from "react";
-import { useGetUserQuery } from "../../services/GetUserService";
-import { useDispatch } from "react-redux";
-import { getUserInfo } from "../../features/UserInfo/UserInfo";
-import { useNavigate } from "react-router-dom";
+import {useEffect, useState} from "react";
+import {useGetUserQuery} from "../../services/GetUserService";
+import {useDispatch} from "react-redux";
+import {getUserInfo} from "../../features/UserInfo/UserInfo";
+import {useNavigate} from "react-router-dom";
 
 const ProfileInfo = () => {
   const [isDropDownMenuOpened, setIsDropDownMenuOpened] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { data } = useGetUserQuery("");
+  const {data} = useGetUserQuery("");
 
   useEffect(() => {
     dispatch(getUserInfo(data));
@@ -34,19 +34,19 @@ const ProfileInfo = () => {
   return (
     <div className={styles.container}>
       <div className={styles.infoWrapper}>
-        <img className={styles.image} src={calendar} alt="календарь" />
-        <p className={styles.text}>{data.current_date}</p>
+        <img className={styles.image} src={calendar} alt="календарь"/>
+        <p className={styles.text}>{data?.current_date}</p>
       </div>
       <div className={styles.infoWrapper}>
-        <img className={styles.image} src={store} alt="магазин" />
-        <p className={styles.text}>{data.store_ids}</p>
+        <img className={styles.image} src={store} alt="магазин"/>
+        <p className={styles.text}>{data?.store_ids}</p>
       </div>
       <button className={styles.button} type="button" onClick={onClick}>
         <div className={styles.infoWrapper}>
-          <img className={styles.image} src={user} alt="личный кабинет" />
+          <img className={styles.image} src={user} alt="личный кабинет"/>
           <p
             className={styles.text}
-          >{`${data.first_name} ${data.last_name}`}</p>
+          >{`${data?.first_name} ${data?.last_name}`}</p>
           <img
             className={
               isDropDownMenuOpened ? styles.turnedImage : styles.arrowImage
